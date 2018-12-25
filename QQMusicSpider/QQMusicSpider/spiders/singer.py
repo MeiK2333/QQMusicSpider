bdd206c82d32a0d43e58dcd14b1823b9ad30c333
MeiK <meik2333@gmail.com>
2018-12-25 10:40:53 +0800

    Update

--- QQMusicSpider/QQMusicSpider/spiders/singer.py
+++ QQMusicSpider/QQMusicSpider/spiders/singer.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from QQMusicSpider.utils import qqmusic_singer_list_url_args_index_and_page, qqmusic_singer_index_all_url
 import json
 
 
@@ -9,18 +8,7 @@ class SingerSpider(scrapy.Spider):
     allowed_domains = ['y.qq.com']
 
     def start_requests(self):
-        self.total = {}
-        for i in range(1, 28):
-            self.total[i] = None
-            yield scrapy.Request(url=qqmusic_singer_list_url_args_index_and_page(i, 0))
+        pass
 
     def parse(self, response):
-        json_response = json.loads(response.body_as_unicode())
-        index = json_response['singerList']['data']['index']
-        if self.total[index] is None:
-            self.total[index] = json_response['singerList']['data']['total']
-            for i in qqmusic_singer_index_all_url(index, self.total[index]):
-                yield scrapy.Request(url=i)
-
-        for i in json_response['singerList']['data']['singerlist']:
-            yield i
+        pass
